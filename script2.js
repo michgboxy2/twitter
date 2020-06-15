d6e137cd5acf797df35ea027c9d96842183836b5
michgboxy <michgboxy@gmail.com>
2020-06-16 00:45:50 +0100

    twitter design clone

---
+++ script2.js
@@ -0,0 +1,108 @@
+// const newEL = document.createElement('li');
+
+// const text = document.createTextNode('Blog');
+
+// newEL.appendChild(text);
+
+// console.log(newEL);
+
+// const list = document.querySelector('.list');
+
+// list.appendChild(newEL);
+
+// list.insertBefore(newEL, list.children[0]);
+
+// list.removeChild(newEL);
+
+
+// console.log(list);
+
+
+
+// const listItem = document.getElementById('list-item');
+
+// console.log(listItem.parentNode);
+
+// const list = document.querySelector('.list');
+// console.log(list.childNodes);
+// console.log(list.children);
+// console.log(list.firstElementChild);
+
+// console.log(listItem.previousElementSibling);
+// console.log(listItem.nextElementSibling);
+
+
+// const paragraph = document.querySelector('.paragraph');
+
+// console.log(paragraph.getAttribute('id'));
+// console.log(paragraph.getAttribute('class'));
+// console.log(paragraph.getAttribute('title'));
+
+// paragraph.setAttribute('style', 'background-color: coral');
+
+// paragraph.removeAttribute('style');
+
+// console.log(paragraph.hasAttribute('style'));
+// console.log(paragraph.hasAttribute('class'));
+
+
+
+// const heading = document.querySelector('.heading');
+// const btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     console.log('clicked'); 
+// }
+
+// btn.onmouseover = () => {
+//     heading.style.backgroundColor = 'green';
+// }
+
+// btn.onmouseout = () => {
+//     heading.style.backgroundColor = 'transparent';
+// }
+
+// function clickMe(){
+//     console.log('clicked');
+// }
+
+// btn.addEventListener('click', () => {
+//     heading.style.cssText = "background-color: brown; color: white";
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading = document.getElementById('heading');
+// heading.className = 'changeBG';
+// // heading.className = 'changeFT';
+// heading.className += ' changeFT'; //add the 2 classes
+// console.log(heading.classList);
+
+// heading.classList.add('changeCL');
+// heading.classList.remove('changeCL');
+
+// console.log(heading.classList);
+
+// const lis = document.querySelectorAll('ul li');
+
+// // console.log(lis);
+
+// for (let i = 0; i < lis.length; i++){
+//     lis[i].style.backgroundColor = 'royalblue';
+// }
+
+
+// lis[0].style.cssText = 'background-color: coral; color: white; font-size: 25px';
